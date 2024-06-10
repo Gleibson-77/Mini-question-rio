f3bc91f6c58d09d160003c4b62eab1f5b233d7c0
Gleibson <102842848+Gleibson-77@users.noreply.github.com>
2024-06-10 11:48:11 -0300

    Add files via upload

---
+++ Miniquest.py
@@ -0,0 +1,9 @@
+nome = input ('Qual é seu nome?')
+print ('Olá', nome, 'Que prazer te ver por aqui!')
+
+idade = input ('Qual é a sua idade?')
+print (idade)
+
+trabalho = input ('Trabalha atulmente?')
+print (trabalho, 'Que legal!')
+
